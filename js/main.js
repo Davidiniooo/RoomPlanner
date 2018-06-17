f778671aedb430255a33c031a36b517ff4174507
Davidiniooo <Davidinioooo@gmail.com>
2018-06-17 21:40:51 +0200

    updatet listing of objects

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 //Variables
+var IDnextObstacle=0;
 var canvas = document.getElementById("mainscreen");
 var ctx = canvas.getContext("2d");
 
@@ -7,8 +8,8 @@ var obstacles = [];
 function drawMainScreen(){
   ctx.beginPath();
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-    for(var i = 0;i<obstacles.length;i++){
-      ctx.rect(obstacles[i].xPos,obstacles[i].yPos,obstacles[i].width,obstacles[i].height);
+  for(var i = 0;i<obstacles.length;i++){
+    ctx.rect(obstacles[i].xPos,obstacles[i].yPos,obstacles[i].width,obstacles[i].height);
   }
   ctx.fill();
   ctx.closePath();
@@ -18,8 +19,9 @@ function addObstacle(){
   var y = document.getElementById("Yinput").value;
   var w = document.getElementById("Winput").value;
   var h = document.getElementById("Hinput").value;
-  var tempObstacle = new Obstacle(x,y,w,h);
+  var tempObstacle = new Obstacle(x,y,w,h,IDnextObstacle);
   obstacles.push(tempObstacle);
+  IDnextObstacle++;
   drawMainScreen();
 }
 function reset(){
@@ -32,21 +34,34 @@ function resize() {
 }
 function sortBySize(){
   var tempArray = obstacles;
-  var sortedAray = [];
-  var biggestSize=0;
-  var indexOfBiggestSize=0;
-  var tempObstacle;
+  var sortedArray = [];
+  alert(obstacles.length);
+  for (var a = 0; a < obstacles.length; a++) {alert(a);}
 
   for (var i = 0; i < obstacles.length; i++) {
-    for (var i = 0; i < tempArray.length; i++) {
-      if(tempArray[i].height+tempArray[i].width>biggestSize)
+    var indexOfBiggestSize=0;
+    var biggestSize=0;
+
+    var arrayLength=tempArray.length;
+    alert(i);
+    for (var y = 0; y < arrayLength; y++) {
+      if(tempArray[y].height*tempArray[y].width>=biggestSize)
       {
-        biggestSize=tempArray[i].height+tempArray[i].width;
-        indexOfBiggestSize=i;
+        biggestSize=tempArray[y].height*tempArray[y].width;
+        indexOfBiggestSize=y;
       }
     }
-    sortedAray.push(tempArray[indexOfBiggestSize]);
+    sortedArray.push(tempArray[indexOfBiggestSize]);
     tempArray.splice(indexOfBiggestSize,1);
   }
-  obstacles = sortedAray;
+  obstacles = sortedArray;
+  listObstacles();
+}
+function listObstacles(){
+  var lastXPos = 0;
+  for(i=0;i<obstacles.length;i++){
+    obstacles[i].xPos=lastXPos;
+    lastXPos +=obstacles[i].width;
+  }
+  drawMainScreen();
 }
--- js/mainclasses.js
+++ js/mainclasses.js
@@ -1,8 +1,14 @@
 class Obstacle {
-  constructor(x,y,w,h) {
+  constructor(x,y,w,h,id) {
     this.xPos    = x;
-    this.yPos  = y;
+    this.yPos   = y;
     this.height = h;
     this.width  = w;
+    this.id     = id;
+    this.xPos=parseInt(this.xPos);
+    this.yPos=parseInt(this.yPos);
+    this.height=parseInt(this.height);
+    this.width=parseInt(this.width);
+
   }
 }
